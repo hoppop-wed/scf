6144ecfc25702b2c413305599618c18981e33117
hoppop-wed <68385729+hoppop-wed@users.noreply.github.com>
2020-08-12 18:02:00 +0800

    Update scf.js

--- scf.js
+++ scf.js
@@ -37,7 +37,6 @@ const FONT_PINGYONG = HEREDOC(() => {/*
 
 //社会价值观、
 var a_idx = 0;
-jQuery(document).ready(function($) {
 $("body").click(function(e) {
 var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
 var $i = $("<span />").text(a[a_idx]);
@@ -62,7 +61,7 @@ function() {
 $i.remove();
 });
 });
-});
+
 
 
 // 雨伞背景
